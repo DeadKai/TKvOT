2dd6055e2069eec2c3d62609cffd793bb944a5fa
DeadKai <kaido.kossas@gmail.com>
2023-11-17 16:45:21 +0200

    muudatused

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,3 @@
-const myHeading = document.querySelector("h1");
-
-myHeading.onclick = () => {
-    const myText = myHeading.textContent;
-    if (myText === "Programmeerimiskeeli tutvustav lehekülg") {
-        myHeading.textContent = "Easter egg";
-    } else {
-        myHeading.textContent = "Programmeerimiskeeli tutvustav lehekülg";    
-    }
-}
-
 const toggleSwitch = document.getElementById("dark-mode-toggle"); 
 
 function switchBackround() {
