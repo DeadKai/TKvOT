9fd1291742e96dbc1f79cbf2db92ae1bc54f0137
DeadKai <kaido.kossas@gmail.com>
2023-12-03 20:23:21 +0200

    muudatused 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-const toggleSwitch = document.getElementById("dark-mode-toggle"); 
+const toggleSwitch = document.getElementById("dark-mode-toggle");
 
-function switchBackround() {
-    var body = document.body
-    if (toggleSwitch.checked) {
+function setTheme(theme) {
+    const body = document.body;
+    if (theme === "dark") {
         body.style.backgroundColor = "#5C616B";
         body.style.color = "#E5CCCA";
     } else {
@@ -11,4 +11,23 @@ function switchBackround() {
     }
 }
 
-toggleSwitch.addEventListener("change", switchBackround);
+function toggleTheme() {
+    const isDarkMode = toggleSwitch.checked;
+    const theme = isDarkMode ? "dark" : "light";
+    
+    setTheme(theme);
+
+    localStorage.setItem("theme", theme);
+}
+
+function loadSavedTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        toggleSwitch.checked = savedTheme === "dark";
+        setTheme(savedTheme);
+    }
+}
+
+toggleSwitch.addEventListener("change", toggleTheme);
+
+loadSavedTheme();
