650969240b5e1058c37119c73acb66bdef74204c
leokas <pole@vaja.seda>
2023-12-04 17:18:43 +0200

    main.js commentid

--- scripts/main.js
+++ scripts/main.js
@@ -4,25 +4,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const iconSun = toggle.querySelector('.fa-sun');
     const body = document.body;
 
+    //kontrollib kas leht on dark mode's
     const isDarkMode = localStorage.getItem('darkMode') === 'true';
 
+    // Kui leht on dark mode'is, lisa vastav klass body-le ja pane kuu ikoon nähtavaks
     if (isDarkMode) {
         body.classList.add('dark-mode');
         iconMoon.style.display = 'inline-block';
         iconSun.style.display = 'none';
     }
 
+    // Lisab EventListener-i dark mode lülitamise jaoks
     toggle.addEventListener('click', function() {
+        // Vaheta body dark mode klass
         body.classList.toggle('dark-mode');
 
-        if (body.classList.contains('dark-mode')) {
-            iconMoon.style.display = 'inline-block';
+        if (body.classList.contains('dark-mode')) { 
+            iconMoon.style.display = 'inline-block'; // Kui body sisaldab darkmode klassi vaheta ikoon kuuks
             iconSun.style.display = 'none';
-            localStorage.setItem('darkMode', 'true');
+            localStorage.setItem('darkMode', 'true'); // Salvesta dark mode'i seade
         } else {
             iconMoon.style.display = 'none';
-            iconSun.style.display = 'inline-block';
-            localStorage.setItem('darkMode', 'false');
+            iconSun.style.display = 'inline-block'; // Kui body ei sisalda darkmode klassi vaheta ikoon päikeseks
+            localStorage.setItem('darkMode', 'false'); // Salvesta dark mode'i seade
         }
     });
 });
