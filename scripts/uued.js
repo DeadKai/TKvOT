98ed6bfd2232bb891b283a079e36fe0adf848155
DeadKai <kaido.kossas@gmail.com>
2023-12-04 16:00:14 +0200

    backend website ja paar fixi

--- scripts/uued.js
+++ scripts/uued.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var rustLogo = document.getElementById('rust-logo');
     
     rustLogo.addEventListener('click', function() {
-        // Add the rotating class to start the animation
         this.classList.add('rotating');
 
-        // Optional: Remove the class after the animation completes (2 seconds) to allow re-click animation
         setTimeout(() => {
             this.classList.remove('rotating');
         }, 2000);
